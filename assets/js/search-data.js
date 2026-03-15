508c7b650d70a9701b57a9e5e252d7b1205b9c5d
dr-yunfei-zhang <dr.yunfei.zhang@gmail.com>
2026-03-15 05:19:27 +0000

    Deploying to gh-pages from @ dr-yunfei-zhang/dr-yunfei-zhang.github.io@9c02211480b0dc4dd912fee8f5fafb4184df4bba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "academics and students engaged in BIG-Culture",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "nav-cv",
           title: "CV",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
